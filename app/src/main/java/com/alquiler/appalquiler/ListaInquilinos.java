73b40aab80d46a83a20c979ed8623336335dcb39
kevinQuispe <kquispe07@hotmail.com>
2017-11-14 20:48:02 -0500

    Cleaning code

--- app/src/main/java/com/alquiler/appalquiler/ListaInquilinos.java
+++ app/src/main/java/com/alquiler/appalquiler/ListaInquilinos.java
@@ -1,22 +1,15 @@
 package com.alquiler.appalquiler;
-
-import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import com.alquiler.appalquiler.entidades.persona;
 import com.alquiler.appalquiler.utilidades.Utilidades;
 
